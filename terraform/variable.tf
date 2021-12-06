78d6dcbff0d19c347b59e7cecb538deff9335e8b
sksat <sksat@sksat.net>
2021-12-07 07:06:41 +0900

    fix: use private_key instead of private_key_path

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,7 +1,7 @@
 variable "OCID_TENANCY" {}
 variable "OCID_USER" {}
 variable "OCID_FINGERPRINT" {}
-variable "OCID_PRIVATE_KEY_PATH" {}
+variable "OCID_PRIVATE_KEY" {}
 variable "OCID_COMPARTMENT" {}
 variable "OCID_SUBNET" {}
 variable "OCID_SOURCE" {}
