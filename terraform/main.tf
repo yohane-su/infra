2548990b85905649e4b5b711a4cbc29a5b368332
sksat <sksat@sksat.net>
2021-12-07 04:46:26 +0900

    update: use remote backend(Terraform Cloud)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+	backend "remote" {
+		organization = "sksat"
+
+		workspaces {
+			name = "yohanesu-infra"
+		}
+	}
+}
+
 resource "oci_core_instance" "generated_oci_core_instance" {
 	agent_config {
 		is_management_disabled = "false"
