81b7811eaf5e7097dc74faa16ee792d4f6e3b9be
sksat <sksat@sksat.net>
2021-12-08 12:10:56 +0900

    add: current ingress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,3 +75,48 @@ resource "oci_core_security_list" "egress_rule" {
     stateless        = false
   }
 }
+
+resource "oci_core_security_list" "ingress_icmp" {
+  compartment_id = var.OCID_COMPARTMENT
+  vcn_id         = var.OCID_VCN
+
+  ingress_security_rules {
+    protocol    = "1"
+    source_type = "CIDR_BLOCK"
+    source      = "0.0.0.0/0"
+    stateless   = false
+
+    icmp_options {
+      type = 3
+      code = 4
+    }
+  }
+  ingress_security_rules {
+    protocol    = "1"
+    source_type = "CIDR_BLOCK"
+    source      = "10.0.0.0/16"
+    stateless   = false
+
+    icmp_options {
+      type = 3
+      code = -1
+    }
+  }
+}
+
+resource "oci_core_security_list" "ingress_ssh" {
+  compartment_id = var.OCID_COMPARTMENT
+  vcn_id         = var.OCID_VCN
+
+  ingress_security_rules {
+    protocol    = "6"
+    source_type = "CIDR_BLOCK"
+    source      = "0.0.0.0/0"
+    stateless   = false
+
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+}
