3a7ee0556ce4b3b5d0aaf10475a2741ecce50edb
sksat <sksat@sksat.net>
2021-12-12 01:48:13 +0900

    add: E2.1.Micro instance(Ubuntu minimal)

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -45,3 +45,63 @@ resource "oci_core_instance" "generated_oci_core_instance" {
   }
 }
 
+resource "oci_core_instance" "x86_test" {
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
+  availability_domain = "dhYs:AP-TOKYO-1-AD-1"
+  compartment_id      = var.OCID_COMPARTMENT
+  create_vnic_details {
+    assign_private_dns_record = "true"
+    assign_public_ip          = "true"
+    subnet_id                 = oci_core_subnet.ubuntu_subnet.id
+  }
+  display_name = "ubuntu-x86-01"
+  instance_options {
+    are_legacy_imds_endpoints_disabled = "false"
+  }
+  is_pv_encryption_in_transit_enabled = "true"
+  metadata = {
+    "ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs3X141dqN0IgcRzUo/0j1XVmy5/BcDD8RrXwygxdaX sksat@yohanesu"
+  }
+  shape = "VM.Standard.E2.1.Micro"
+  shape_config {
+    memory_in_gbs = 1
+    ocpus         = 1
+  }
+  source_details {
+    boot_volume_size_in_gbs = "50"
+    source_id               = oci_core_image.x86_ubuntu_minimal.id
+    source_type             = "image"
+  }
+}
+
+resource "oci_core_image" "x86_ubuntu_minimal" {
+  compartment_id = var.OCID_COMPARTMENT
+
+  display_name = "x86 image"
+  launch_mode  = "NATIVE"
+
+  image_source_details {
+    source_type = "objectStorageTuple"
+
+    operating_system         = "Canonical Ubuntu"
+    operating_system_version = "20.04 Minimal"
+  }
+}
