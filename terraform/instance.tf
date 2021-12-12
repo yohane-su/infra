c9d62f3e8dcf0a97e5d9daaaf71ffe5c05d578d4
sksat <sksat@sksat.net>
2021-12-12 17:57:18 +0900

    add x86 instance

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -60,3 +60,66 @@ resource "oci_core_instance" "generated_oci_core_instance" {
     "ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs3X141dqN0IgcRzUo/0j1XVmy5/BcDD8RrXwygxdaX sksat@yohanesu"
   }
 }
+
+resource "oci_core_instance" "x86_test_instance" {
+  compartment_id      = var.OCID_COMPARTMENT
+  availability_domain = "dhYs:AP-TOKYO-1-AD-1"
+  fault_domain        = "FAULT-DOMAIN-2"
+
+  display_name = "x86-ubuntu-01"
+  shape        = "VM.Standard.E2.1.Micro"
+
+  shape_config {
+    memory_in_gbs = "1"
+    ocpus         = "1"
+  }
+
+  source_details {
+    source_type             = "image"
+    boot_volume_size_in_gbs = "50"
+
+    # Ubuntu 20.04 Minimal
+    # https://docs.oracle.com/en-us/iaas/images/image/cc6e4086-515b-4254-99ea-171acc5e7460/
+    source_id = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaato55by5l2yabyitrv62utowyisopehtvqbjrui6vympbi5b57k7q"
+  }
+
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
+
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
+
+  create_vnic_details {
+    assign_private_dns_record = "true"
+    assign_public_ip          = "true"
+    subnet_id                 = var.OCID_SUBNET
+  }
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = "false"
+  }
+
+  is_pv_encryption_in_transit_enabled = "true"
+
+  metadata = {
+    "ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs3X141dqN0IgcRzUo/0j1XVmy5/BcDD8RrXwygxdaX sksat@yohanesu"
+  }
+}
