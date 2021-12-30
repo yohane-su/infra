04db626ecd364ede1e98187464d2e7318ab1a7a7
sksat <sksat@sksat.net>
2021-12-30 13:08:41 +0900

    add: compose-switch to docker-compose cookbook, start ob-server

--- itamae/cookbooks/docker-compose/default.rb
+++ itamae/cookbooks/docker-compose/default.rb
@@ -4,7 +4,10 @@ package 'docker.io'
 
 COMPOSE_VERSION="v2.2.2"
 COMPOSE_ARCH="x86_64"
+COMPOSE_SWITCH_ARCH="amd64" # 合わせんかい
 COMPOSE_BIN="https://github.com/docker/compose/releases/download/#{COMPOSE_VERSION}/docker-compose-linux-#{COMPOSE_ARCH}"
+
+COMPOSE_HOME="/root/"
 
 define :download_github_binary, tag: nil, file: nil, target: nil  do
   project = params[:name]
@@ -37,6 +40,9 @@ define :install_github_binary, tag: nil, file: nil, target: nil  do
   if p_target.nil?
     p_target = "/usr/local/bin/#{p_file}"
   end
+  if not p_target.include? "/"
+    p_target = "/usr/local/bin/#{p_target}"
+  end
 
   p_tag = params[:tag]
   download_github_binary params[:name] do
@@ -50,10 +56,16 @@ end
 
 # install docker-compose(v2)
 # https://docs.docker.com/compose/cli-command/#install-on-linux
-run_command("mkdir -p /home/ubuntu/.docker/cli-plugins")
+run_command("mkdir -p #{COMPOSE_HOME}/.docker/cli-plugins")
 #directory "/home/ubuntu/.docker/cli-plugins" # なぜか動かない！！！
 
 install_github_binary 'docker/compose' do
   file "docker-compose-linux-#{COMPOSE_ARCH}"
-  target "/home/ubuntu/.docker/cli-plugins/docker-compose"
+  target "#{COMPOSE_HOME}/.docker/cli-plugins/docker-compose"
+end
+
+# MEMO: compose-switchを使おうがcli pluginとして入ったdocker-composeが呼ばれるのでcomposeを適切に配置する必要がある
+install_github_binary 'docker/compose-switch' do
+  file "docker-compose-linux-#{COMPOSE_SWITCH_ARCH}"
+  target "docker-compose"
 end
--- itamae/cookbooks/ob-server/default.rb
+++ itamae/cookbooks/ob-server/default.rb
@@ -1,3 +1,10 @@
-git "/home/ubuntu/srv/ob-server" do
+SRV_DIR="/home/ubuntu/srv/ob-server"
+
+git SRV_DIR do
   repository "https://github.com/sksat/ob-server.git"
+end
+
+execute "start ob-server" do
+  cwd SRV_DIR
+  command "docker-compose up -d"
 end
