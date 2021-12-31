3027b0ba67a45cd46918b12ffd371b59a9854d9a
sksat <sksat@sksat.net>
2021-12-31 18:29:59 +0900

    add: use hocho & mitamae

--- itamae/cookbooks/ssh/default.rb
+++ itamae/cookbooks/ssh/default.rb
@@ -11,18 +11,12 @@ http_request "#{HOME_DIR}/.ssh/sksat.keys" do
   url "https://github.com/sksat.keys"
 end
 
-remote_file "#{HOME_DIR}/.ssh/deploy.keys" do
-  owner "ubuntu"
-  source "../../../deploy.pub"
-end
-
 file "#{HOME_DIR}/.ssh/authorized_keys" do
   action :create
   mode "600"
   cwd "#{HOME_DIR}/.ssh"
   block do |content|
     merged_keys = run_command("cat #{HOME_DIR}/.ssh/authorized_keys").stdout
-    merged_keys +=run_command("cat #{HOME_DIR}/.ssh/deploy.keys").stdout
     merged_keys +=run_command("cat #{HOME_DIR}/.ssh/sksat.keys").stdout
     merged_keys = merged_keys.split("\n").uniq.join("\n") + "\n"
 
